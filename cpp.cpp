bfe9ed4da2f93ef0f000c0d2fa62d4d2f702379c
defMyself <anxinyucomputer@163.com>
2020-01-04 16:13:11 +0800

    ComparingWords

---
+++ cpp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    const string s1 = "COMPUTE";
+    const string s2 = "COMPUTER";
+    const string s3 = "COMPUTABLE";
+    const string s4 = "COMPUTARE";
+    printComparison(s1,s2);
+    printComparison(s1,s3);
+}
